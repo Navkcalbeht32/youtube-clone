1afbd56585f54206d030ee96187b4483ddbb55d6
Navkcalbeht32 <asumoski@gmail.com>
2021-01-11 21:17:02 -0500

    youtube clone create react app, material ui, header component build

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.styles.scss';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AppsIcon from '@material-ui/icons/Apps';
+import Avatar from '@material-ui/core/Avatar';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-left">
+                <MenuIcon />
+                <img
+                className="header-logo" 
+                src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt=""/>
+            </div>
+            <div className="header-input">
+                <input type="text" placeholder="Search" />
+                <SearchIcon className="header-input-button" />
+            </div>
+            <div className="header-icons">
+                <VideoCallIcon className="header-icon" />
+                <AppsIcon className="header-icon" />
+                <NotificationsIcon className="header-icon" />
+                <Avatar className="header-icon" />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
