522e69bc98d1de68a19530b0cb74512aa96f98d6
Navkcalbeht32 <asumoski@gmail.com>
2021-01-13 19:47:14 -0500

    video card changes

--- src/components/VideoCard/VideoCard.component.jsx
+++ src/components/VideoCard/VideoCard.component.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './VideoCard.styles.scss';
 import Avatar from '@material-ui/core/Avatar';
-
+import Tooltip from '@material-ui/core/Tooltip';
 
 
 const VideoCard = ( {title, image, time, channel, channelImage, views, timestamp} ) => {
     return (
         <div className="video-card">
-                <img src={image} alt="" className="thumbnail-image" />
+                <a href="">
+                    <img src={image} alt="" className="thumbnail-image" />
+                </a>
             <div className="video-info">
                 <Avatar className="video-avatar" alt={channel} src={channelImage} />
                 <div className="video-text">
-                    <h4>{title}</h4>
-                    <h5>{channel}</h5>
+                    <a href="">
+                        <h4>{title}</h4>
+                    </a>
+                
+                    <a href="#" className="channel-link">
+                        <Tooltip title={channel}>
+                            <h5>{channel}</h5>
+                        </Tooltip>
+                    </a>
+                    
                     <p>{views} views • {timestamp} </p>
     
                 </div>
