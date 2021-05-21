3b72ffa5c542761cc9bea4f0330c042067aa9cfb
Navkcalbeht32 <asumoski@gmail.com>
2021-05-21 13:33:21 -0400

    Oauth verification

--- src/components/SearchPage/Search/search.js
+++ src/components/SearchPage/Search/search.js
@@ -4,12 +4,12 @@ const { google } = require('googleapis');
 google.youtube('v3').search.list({
     key: process.env.REACT_APP_YOUTUBE_KEY,
     part: 'snippet',
-    q: 'vivaldi',
+    q: 'vivald',
     maxResults: 10
 }).then((response) => {
     const { data } = response;
     data.items.forEach((item) => {
-        console.log(`Title: ${item.snippet.thumbnails}\n `);
+        console.log(`Title: ${item.snippet.title }\n `);
     })
 }).catch((err) => console.log(err));
 
